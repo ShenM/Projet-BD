7badf38d724529e74e6d138c916bc307482e42a1
Shen Mesrobian <Epulapp@EPULAPP78.univ-lyon1.fr>
2015-11-10 11:23:18 +0100

    Authentification/Redirection

--- ProjetBD/src/com/sdzee/servlets/Accueil.java
+++ ProjetBD/src/com/sdzee/servlets/Accueil.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.sdzee.beans.Beneficiaire;
 import com.sdzee.beans.PrestationsSante;
@@ -17,17 +18,29 @@ import com.sdzee.dao.PrestationsSanteDAOImpl;
 public class Accueil extends HttpServlet {
     public static final String LISTE_PRESTA = "lpresta";
     public static final String BENEFICIAIRE = "benef";
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
     
     
-	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{		
-		PrestationsSanteDAOImpl prestaDAOImpl = new PrestationsSanteDAOImpl(DAOFactory.getInstance());
-		BeneficiaireDAOImpl benefiDAOImpl = new BeneficiaireDAOImpl(DAOFactory.getInstance());
-		Beneficiaire benef = new Beneficiaire();
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+		if (request.getSession()!=null && request.getSession().getAttribute(ATT_SESSION_USER)!=null){
+			
+			HttpSession session = request.getSession();
+			
+			PrestationsSanteDAOImpl prestaDAOImpl = new PrestationsSanteDAOImpl(DAOFactory.getInstance());
+			BeneficiaireDAOImpl benefiDAOImpl = new BeneficiaireDAOImpl(DAOFactory.getInstance());
+			Beneficiaire benef = new Beneficiaire();		
+			benef = (Beneficiaire) session.getAttribute(ATT_SESSION_USER);
+			ArrayList<PrestationsSante> prestaListe = prestaDAOImpl.trouverParNumBeneficiaireSinistre(benef.getNum());
+			
+			request.setAttribute(LISTE_PRESTA, prestaListe);
+			request.setAttribute(BENEFICIAIRE, benef);
+			
+			this.getServletContext().getRequestDispatcher( "/WEB-INF/Accueil.jsp" ).forward( request, response );
+			
+		}else {
+			response.sendRedirect("/ProjetBD/Authentification");
+		}
 		
-		benef = benefiDAOImpl.trouver(2484);
-		ArrayList<PrestationsSante> prestaListe = prestaDAOImpl.trouverParNumBeneficiaireSinistre(2484);
-		request.setAttribute(LISTE_PRESTA, prestaListe);
-		request.setAttribute(BENEFICIAIRE, benef);
-		this.getServletContext().getRequestDispatcher( "/WEB-INF/Accueil.jsp" ).forward( request, response );
+		
 	}
 }
